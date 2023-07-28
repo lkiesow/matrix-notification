47a550ef43d50dbd7fbecf46ce550b0f61ffc668
Lars Kiesow <lkiesow@uos.de>
2023-07-28 23:29:06 +0200

    Add matrix-msg binary
    This patch adds the option to have this action prepare a binary to
    easily send messages instead of sending a message directly. The binary
    can be used either with the message as the first argument:
    
        matrix-msg "The message"
    
    Or by piping the message into the tool:
    
        echo The message | matrix-msg

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import * as core from '@actions/core'
 import axios from 'axios'
+import {homedir} from 'os'
+import {mkdirSync, writeFileSync} from 'fs'
+
+function generate_tool(url: string): string {
+  return `#!/bin/bash
+    set -eu
+    if test "$#" -lt 1; then
+      read body
+    else
+      body="\${1}"
+    fi
+    jq --arg body "\${body}" '.body = $body' <<< '{"msgtype": "m.text", "body": null}' |\
+      curl -s -XPOST -H "Content-Type: application/json" --data-binary @- "${url}"`
+}
 
 async function run(): Promise<void> {
   try {
@@ -7,18 +21,29 @@ async function run(): Promise<void> {
     const server: string = core.getInput('server')
     const room: string = core.getInput('room')
     const message: string = core.getInput('message')
+    const tool: boolean = core.getBooleanInput('tool')
 
     const encodedRoom = encodeURI(room)
     const url = `https://${server}/_matrix/client/r0/rooms/${encodedRoom}/send/m.room.message?access_token=${token}`
-    const body = {
-      msgtype: 'm.text',
-      body: message
+
+    if (tool) {
+      core.info('Installing matrix-message binary')
+      const script = generate_tool(url)
+      const home = homedir()
+      mkdirSync(`${home}/.local/bin/`, {recursive: true})
+      writeFileSync(`${home}/.local/bin/matrix-msg`, script, {mode: 0o755})
     }
 
-    core.info('Sending message')
-    const {data, status} = await axios.post(url, body)
-    core.info(`status: ${status}`)
-    core.debug(`status: ${JSON.stringify(data)}`)
+    if (message) {
+      core.info('Sending message')
+      const body = {
+        msgtype: 'm.text',
+        body: message
+      }
+      const {data, status} = await axios.post(url, body)
+      core.info(`status: ${status}`)
+      core.debug(`status: ${JSON.stringify(data)}`)
+    }
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
