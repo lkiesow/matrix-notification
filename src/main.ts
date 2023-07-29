5151ee85f1a29130e88bfc482c4c04262d9c2f2c
Lars Kiesow <lkiesow@uos.de>
2023-07-29 14:25:28 +0200

    Option to send formatted messages
    This patch allows for sending formatted messages in addition to the
    plain text ones with either the action directly or with the `matrix-msg`
    binary.
    
    If no plain text version is provided. The code will do a best effort
    approach to generating one from the formatted message.

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,15 @@ function generate_tool(url: string): string {
     else
       body="\${1}"
     fi
-    jq --arg body "\${body}" '.body = $body' <<< '{"msgtype": "m.text", "body": null}' |\
-      curl -s -XPOST -H "Content-Type: application/json" --data-binary @- "${url}"`
+    if test "$#" -lt 2; then
+      echo '{"msgtype": "m.text", "body": null}' |\
+        jq --arg body "\${body}" '.body = $body' |\
+        curl -s -XPOST -H "Content-Type: application/json" --data-binary @- "${url}"
+    else
+      echo '{"msgtype": "m.text", "body":" ", "format": "org.matrix.custom.html", "formatted_body": null}' |\
+        jq --arg b "\${1}" --arg f "\${2}" '.body = $b | .formatted_body = $f' |\
+        curl -s -XPOST -H "Content-Type: application/json" --data-binary @- "${url}"
+    fi`
 }
 
 async function run(): Promise<void> {
@@ -20,7 +27,8 @@ async function run(): Promise<void> {
     const token: string = core.getInput('token')
     const server: string = core.getInput('server')
     const room: string = core.getInput('room')
-    const message: string = core.getInput('message')
+    let message: string = core.getInput('message')
+    const formatted_message: string = core.getInput('formatted_message')
     const tool: boolean = core.getBooleanInput('tool')
 
     const encodedRoom = encodeURI(room)
@@ -34,11 +42,23 @@ async function run(): Promise<void> {
       writeFileSync(`${home}/.local/bin/matrix-msg`, script, {mode: 0o755})
     }
 
+    // if there is just a formatted message, try building a plain-text version
+    if (formatted_message && !message) {
+      core.info(
+        'Plain text message missing. Building one from formatted message.'
+      )
+      message = formatted_message.replace(/<[^>]+>/g, '')
+    }
+
     if (message) {
       core.info('Sending message')
-      const body = {
+      const body: Record<string, string> = {
         msgtype: 'm.text',
         body: message
+      }
+      if (formatted_message) {
+        body.format = 'org.matrix.custom.html'
+        body.formatted_body = formatted_message
       }
       const {data, status} = await axios.post(url, body)
       core.info(`status: ${status}`)
